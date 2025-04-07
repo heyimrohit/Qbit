6530a6aca252df9db4a4081ffd20fd492d5e4669
imrohituikey <rohituaike81@gmail.com>
2025-04-08 02:01:29 +0530

    Routing implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements ,Route} from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import HomeScreen from "./screens/HomeScreen/HomeScreen.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+
+      <Route path="contact" element={<HomeScreen/>} />
+      
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
