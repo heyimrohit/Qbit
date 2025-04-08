f9aa116a42922ef15d16423964c1b4c0537902af
imrohituikey <rohituaike81@gmail.com>
2025-04-08 11:42:45 +0530

    navbar implemanted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Container from '../Container/Container'
+import './Header.css'
 
 const Header = () => {
   return (
     <Container>
-      <header className='nav'>
-        <div className="nav-left">left</div>
-        <div className="nav-right">right</div>
+      <header className='navbar'>
+        <div className="nav-left">
+          {/* <img src="" alt="" /> */}
+          <span>Logo</span>
+        </div>
+        <div className="nav-right">
+          <ul className='nav-right-list'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+          </ul>
+          
+        </div>
       </header>
     </Container>
   )
